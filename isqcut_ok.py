73495f8151a8fcdcf0a7092abe241bd1655198e5
Esad Demirel <84718982+demirelesad@users.noreply.github.com>
2023-07-28 11:18:22 +0300

    Update and rename qcut.py to isqcut_ok.py

--- isqcut_ok.py
+++ isqcut_ok.py
@@ -1,13 +1,8 @@
 import numpy as np
-from datetime import datetime
 import datetime as dt
-import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.stats import ttest_1samp, shapiro, levene, ttest_ind, mannwhitneyu, \
-    pearsonr, spearmanr, kendalltau, f_oneway, kruskal
+from scipy.stats import ttest_1samp, shapiro, levene, f_oneway, kruskal
 from statsmodels.stats.multicomp import MultiComparison
-import warnings
-warnings.filterwarnings("ignore")
 
 class color:
    PURPLE = '\033[95m'
@@ -20,35 +15,6 @@ class color:
    BOLD = '\033[1m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
-
-pd.set_option('display.max_columns', None)
-# pd.set_option('display.max_rows', None)
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-
-df_ = pd.read_csv(r'C:\Users\ahmed.demirel\PycharmProjects\BizimToptanSecond\escut()_function\flo_data_20k.csv')
-df = df_.copy()
-
-
-df["TotalBuy"] = df["order_num_total_ever_online"] + df["order_num_total_ever_offline"]
-df["TotalPrice"] = df["customer_value_total_ever_offline"] + df["customer_value_total_ever_online"]
-df["first_order_date"] = pd.to_datetime(df["first_order_date"])
-df["last_order_date"] = pd.to_datetime(df["last_order_date"])
-df["last_order_date_online"] = pd.to_datetime(df["last_order_date_online"])
-df["last_order_date_offline"] = pd.to_datetime(df["last_order_date_offline"])
-
-today_date = dt.datetime(year=2021, month=12, day=31)
-today_date = pd.to_datetime(today_date)
-
-
-rfm = df[["master_id", "TotalBuy", "TotalPrice", "last_order_date"]]
-rfm["recency"] = [(today_date - col).days for col in rfm["last_order_date"]]
-rfm = rfm.drop("last_order_date", axis=1)
-rfm = rfm[["master_id", "recency", "TotalBuy", "TotalPrice"]]
-rfm.columns = ["master_id", 'recency', 'frequency', 'monetary']
-rfm = rfm[rfm["monetary"] > 0]
-
-# GÖREV 3: RF ve RFM Skorlarının Hesaplanması
-
 
 def isqcut_ok(series,q=5, ):
     if q in [3, 4, 5, 6]:
@@ -178,76 +144,3 @@ def isqcut_ok(series,q=5, ):
                     print(color.BOLD + color.RED + "Bu grupların ortalaması arasında istatistiksel olarak anlamlı bir farklılık yoktur." + color.END)
     else:
         print(color.BOLD + color.DARKCYAN + "Lütfen q değerini 3-4-5-6 sayılarından seçiniz.." + color.END)
-
-
-ornek = pd.Series([5, 2] * 50)
-isqcut_ok(rfm["recency"], 3)
-isqcut_ok(rfm["frequency"], 5)
-isqcut_ok(rfm["monetary"], 5)
-isqcut_ok(ornek, 2)
-
-
-
-
-
-
-rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
-# 0-100, 0-20, 20-40, 40-60, 60-80, 80-100
-
-rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
-
-rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
-
-rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
-                    rfm['frequency_score'].astype(str))
-
-
-# GÖREV 4: RF Skorlarının Segment Olarak Tanımlanması
-
-
-# RFM isimlendirmesi
-seg_map = {
-    r'[1-2][1-2]': 'hibernating',
-    r'[1-2][3-4]': 'at_Risk',
-    r'[1-2]5': 'cant_loose',
-    r'3[1-2]': 'about_to_sleep',
-    r'33': 'need_attention',
-    r'[3-4][4-5]': 'loyal_customers',
-    r'41': 'promising',
-    r'51': 'new_customers',
-    r'[4-5][2-3]': 'potential_loyalists',
-    r'5[4-5]': 'champions'
-}
-
-rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
-
-rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg(["mean", "count"])
-
-
-temp_df.loc[temp_df["group"] == 2, "score"].hist()
-
-plt.title("grouped Sütunu Histogramı")
-plt.xlabel("Değerler")
-plt.ylabel("Sıklık")
-plt.show(block=True)
-
-
-liste1 = [16, 19, 20, 21, 22, 23, 24, 25, 26, 14,
-         24, 25, 21, 18, 15, 24, 20, 18, 17, 24,
-         36, 38, 41, 24, 29, 51, 37, 39, 41, 44,
-         58, 55, 59, 47, 61, 64, 39, 62, 45, 53,
-         75, 73, 67, 46, 81, 88, 67, 75, 79, 83]
-
-series1 = pd.Series(liste1)
-
-liste2 = [9, 11, 10, 21, 12, 13, 8, 15, 11, 7,
-         24, 25, 21, 18, 15, 24, 20, 18, 17, 24,
-         36, 48, 41, 24, 29, 51, 37, 39, 41, 44,
-         58, 55, 59, 55, 61, 64, 39, 62, 60, 53,
-         75, 73, 81, 61, 81, 88, 67, 75, 87, 83]
-
-series2 = pd.Series(liste2)
-
-isqcut_ok(series2, 5)
-isqcut_ok(series1, 4)
-
